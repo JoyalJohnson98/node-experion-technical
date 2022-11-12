4b9dbacceea14b41257bd00b513ed8a684da80d5
JoyalJohnson98 <110291375+JoyalJohnson98@users.noreply.github.com>
2022-11-12 21:36:54 +0530

    Add files via upload

---
+++ second.js
@@ -0,0 +1,91 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// module.exports = function greet() {
+//     console.log("Hello from node!");
+//    }
+
+
+// module.exports.greet = function greet (){
+//     console.log("Hello from node!");
+// }
+// module.exports.sayhi = function sayhi (){
+//     console.log("Hello from joes")
+// }
+
+
+
+
+//  module.exports.greet = function greet (){
+//      console.log("Hello from node!");
+//  }
+//  module.exports.sayhi = function sayhi (){
+//      console.log("Hello from joes")
+//  }
+
+// greet = function greet (){
+//          console.log("Hello from node!");
+//       }
+// sayhi = function sayhi (){
+//          console.log("Hello from joes")
+//       }
+// module.exports ={
+//     greet: greet,
+//     sayhi: sayhi
+// }
+
+
+// function greet(){
+//     console.log("Hello from node!");
+// }
+// function sayhi(){
+//     console.log("hi");
+// }
+// module.exports ={
+//     welcome: greet,
+//     sayhi: sayhi
+// }
+    
+
+
+// var abishek = {
+//     fullName: 'Abishek K R'
+// } 
+// exports.person = abishek
+
+
+
+
+
+
+
+
+
+
+
+// export function greet(){
+//     console.log("Hello ");
+// }
+//  export function sayHi(){
+//     console.log("hi");
+//  }
+
+
+//  HelloWorld;
+//  Hello;
+
+ 
